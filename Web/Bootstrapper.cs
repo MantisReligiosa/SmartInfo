17a00f8951a6d40b65e1a8f5d02f3e9b8d265d07
Жуков Алексей <mantisreligiosa78@gmail.com>
2018-11-11 10:52:04 +0400

    Прикрутил css и Images

--- Web/Bootstrapper.cs
+++ Web/Bootstrapper.cs
@@ -1,5 +1,6 @@
 ﻿using Nancy;
 using Nancy.Bootstrapper;
+using Nancy.Conventions;
 using Nancy.TinyIoc;
 
 namespace Web
@@ -8,6 +9,15 @@ namespace Web
     {
         protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
         {
+        }
+
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+            nancyConventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("css", @"css"));
+            nancyConventions.StaticContentsConventions.Add(
+               StaticContentConventionBuilder.AddDirectory("Images", @"Images"));
         }
     }
 }
