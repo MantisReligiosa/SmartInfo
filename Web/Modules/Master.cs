b44497c0d32e49e15f211d0f5ba45be2e0efe089
Жуков Алексей <mantisreligiosa78@gmail.com>
2018-11-14 19:17:28 +0400

    Наполнение элементов главного меню

--- Web/Modules/Master.cs
+++ Web/Modules/Master.cs
@@ -7,6 +7,16 @@ namespace Web.Modules
         public Master()
         {
             Get["/master"] = parameters => View["Views/Home/Master.cshtml"];
+
+            Post["/master/fonts"] = parameters =>
+            {
+                return Response.AsJson(new[] { new{ id = 1, text = "Font1" }, new { id = 2, text = "Font2" } });
+            };
+
+            Post["/master/fontSizes"] = parameters =>
+            {
+                return Response.AsJson(new[] { new { id = 1, text = "8" }, new { id = 2, text = "10" } });
+            };
         }
     }
 }
