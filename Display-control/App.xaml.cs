1f323728566b47f0e2f6c645057f330802049087
Жуков Алексей <mantisreligiosa78@gmail.com>
2019-01-19 21:54:44 +0400

    Запуск просмотра. Текстовый блок

--- Display-control/App.xaml.cs
+++ Display-control/App.xaml.cs
@@ -7,7 +7,10 @@ using System;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using Web;
+using DisplayBlock = DomainObjects.Blocks;
 using Media = System.Windows.Media;
 
 namespace Display_control
@@ -17,19 +20,18 @@ namespace Display_control
     /// </summary>
     public partial class App : Application
     {
-        private MainWindow _window;
-        private MainWindowViewModel _viewModel;
-        private System.Windows.Forms.NotifyIcon _notifyIcon;
+        private Window _window;
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            _notifyIcon = new System.Windows.Forms.NotifyIcon
+
+            var notifyIcon = new System.Windows.Forms.NotifyIcon
             {
                 Icon = Display_control.Properties.Resources.Logo,
                 Visible = true,
                 ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip()
             };
-            _notifyIcon.ContextMenuStrip.Items.Add("Exit").Click += (s, args) => Current.Shutdown();
+            notifyIcon.ContextMenuStrip.Items.Add("Exit").Click += (s, args) => Current.Shutdown();
             var broker = Broker.GetBroker();
             broker.RegisterHandler<GetScreenSizeRequest>(request =>
             {
@@ -64,22 +66,51 @@ namespace Display_control
                     _window.Width = requestData.Screens.Displays.Max(d => d.Left + d.Width) - requestData.Screens.Displays.Min(d => d.Left);
                     _window.Left = requestData.Screens.Displays.Min(d => d.Left);
                     _window.Top = requestData.Screens.Displays.Min(d => d.Top);
-                    _window.Visibility = Visibility.Visible;
                     var bc = new Media.BrushConverter();
-                    string colorHex;
-                    var grid = _window.Content as System.Windows.Controls.Grid;
-                    if (TryToParseRGB(requestData.Background, out colorHex))
+                    var border = new Border();
+                    if (TryToParseRGB(requestData.Background, out string colorHex))
                     {
-                        grid.Background = (Media.Brush)(bc.ConvertFrom(colorHex));
+                        border.Background = (Media.Brush)bc.ConvertFrom(colorHex);
                     }
-                    else if (TryToParseRGBA(requestData.Background, out colorHex))
+                    var canvas = new Canvas();
+
+                    foreach (var block in requestData.Blocks)
                     {
-                        grid.Background = (Media.Brush)(bc.ConvertFrom(colorHex));
+                        if (block is DisplayBlock.TextBlock textBlock)
+                        {
+                            var label = new TextBlock
+                            {
+                                Height = textBlock.Height,
+                                Width = textBlock.Width,
+                                Text = textBlock.Details.Text,
+                                FontSize = textBlock.Details.FontSize,
+                                FontFamily = new Media.FontFamily(textBlock.Details.FontName),
+                                FontWeight = textBlock.Details.Bold ? FontWeights.Bold : FontWeights.Normal,
+                                FontStyle = textBlock.Details.Italic ? FontStyles.Italic : FontStyles.Normal,
+                                LineHeight = textBlock.Details.FontSize * textBlock.Details.FontIndex,
+                                TextAlignment = textBlock.Details.Align == DisplayBlock.Align.Left ? TextAlignment.Left :
+                                    textBlock.Details.Align == DisplayBlock.Align.Center ? TextAlignment.Center :
+                                    textBlock.Details.Align == DisplayBlock.Align.Right ? TextAlignment.Right : TextAlignment.Left
+                            };
+                            if (TryToParseRGB(textBlock.Details.BackColor, out colorHex))
+                            {
+                                label.Background = (Media.Brush)bc.ConvertFrom(colorHex);
+                            }
+                            if (TryToParseRGB(textBlock.Details.TextColor, out colorHex))
+                            {
+                                label.Foreground = (Media.Brush)bc.ConvertFrom(colorHex);
+                            }
+                            Canvas.SetTop(label, textBlock.Top);
+                            Canvas.SetLeft(label, textBlock.Left);
+                            Panel.SetZIndex(label, textBlock.ZIndex);
+                            canvas.Children.Add(label);
+                        }
                     }
+
+                    border.Child = canvas;
+                    _window.Content = border;
                     _window.Show();
                 });
-                _viewModel.Blocks = requestData.Blocks;
-
                 return null;
             });
             broker.RegisterHandler<StopShowRequest>(request =>
@@ -98,18 +129,19 @@ namespace Display_control
             var uri = new Uri("http://localhost:1234");
             var bootstrapper = new Bootstrapper();
             var nancyHost = new NancyHost(bootstrapper, hostConfiguration, uri);
-
-            //var nancyHost = new NancyHost(new Uri("http://localhost:1234"), new Bootstrapper());
             nancyHost.Start();
-            _viewModel = new MainWindowViewModel
+            _window = new Window()
             {
-                WindowState = WindowState.Normal
+                Visibility = Visibility.Hidden,
+                ShowInTaskbar = false,
+                WindowStyle = WindowStyle.None,
+                Cursor = Cursors.None,
+                ResizeMode = ResizeMode.NoResize,
+#if !DEBUG
+                Topmost = true
+#endif
             };
-            _window = new MainWindow(_viewModel)
-            {
-                Visibility = Visibility.Hidden
-            };
-            _notifyIcon.ShowBalloonTip(1, "Display-control", "Сервер запущен и готов к работе", System.Windows.Forms.ToolTipIcon.Info);
+            notifyIcon.ShowBalloonTip(1, "Display-control", "Сервер запущен и готов к работе", System.Windows.Forms.ToolTipIcon.Info);
         }
 
         private bool TryToParseRGB(string colorString, out string colorHexString)
@@ -123,33 +155,29 @@ namespace Display_control
                 var red = Convert.ToByte(group[1].Value);
                 var green = Convert.ToByte(group[2].Value);
                 var blue = Convert.ToByte(group[3].Value);
-                colorHexString = $"#{red:x}{green:x}{blue:x}";
+                colorHexString = $"#{red:x2}{green:x2}{blue:x2}";
                 return true;
             }
             else
             {
-                return false;
-            }
-        }
-        private bool TryToParseRGBA(string colorString, out string colorHexString)
-        {
-            var regular = @"^rgba\(\s?(\d{1,3})\,\s?(\d{1,3})\,\s?(\d{1,3})\,\s?(0\.\d{1,2})\)$";
-            colorHexString = string.Empty;
-            if (Regex.IsMatch(colorString, regular))
-            {
-                var t = Regex.Matches(colorString, regular);
-                var group = t[0].Groups;
-                var red = Convert.ToByte(group[1].Value);
-                var green = Convert.ToByte(group[2].Value);
-                var blue = Convert.ToByte(group[3].Value);
-                var opacity = Convert.ToDouble(group[4].Value.Replace(".", ","));
-                var opacityByte = Convert.ToByte(Math.Truncate(opacity * 0xff));
-                colorHexString = $"#{opacityByte:x2}{red:x2}{green:x2}{blue:x2}";
-                return true;
-            }
-            else
-            {
-                return false;
+                regular = @"^rgba\(\s?(\d{1,3})\,\s?(\d{1,3})\,\s?(\d{1,3})\,\s?(0\.\d{1,2})\)$";
+                colorHexString = string.Empty;
+                if (Regex.IsMatch(colorString, regular))
+                {
+                    var t = Regex.Matches(colorString, regular);
+                    var group = t[0].Groups;
+                    var red = Convert.ToByte(group[1].Value);
+                    var green = Convert.ToByte(group[2].Value);
+                    var blue = Convert.ToByte(group[3].Value);
+                    var opacity = Convert.ToDouble(group[4].Value.Replace(".", ","));
+                    var opacityByte = Convert.ToByte(Math.Truncate(opacity * 0xff));
+                    colorHexString = $"#{opacityByte:x2}{red:x2}{green:x2}{blue:x2}";
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
             }
         }
     }
