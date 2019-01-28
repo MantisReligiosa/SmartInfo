3da67058cb235f88001903cd6cbdbde6767b50ac
Алексей Жуков <mantis-religiosa@yandex.ru>
2019-01-28 22:08:35 +0400

    Запуск просмотра. Блок с таблицей

--- Display-control/App.xaml.cs
+++ Display-control/App.xaml.cs
@@ -83,94 +83,15 @@ namespace Display_control
                     {
                         if (block is DisplayBlock.TextBlock textBlock)
                         {
-                            var label = new TextBlock
-                            {
-                                Height = textBlock.Height,
-                                Width = textBlock.Width,
-                                Text = textBlock.Details.Text,
-                                FontSize = textBlock.Details.FontSize,
-                                FontFamily = new Media.FontFamily(textBlock.Details.FontName),
-                                FontWeight = textBlock.Details.Bold ? FontWeights.Bold : FontWeights.Normal,
-                                FontStyle = textBlock.Details.Italic ? FontStyles.Italic : FontStyles.Normal,
-                                LineHeight = textBlock.Details.FontSize * textBlock.Details.FontIndex,
-                                TextAlignment = textBlock.Details.Align == DisplayBlock.Align.Left ? TextAlignment.Left :
-                                    textBlock.Details.Align == DisplayBlock.Align.Center ? TextAlignment.Center :
-                                    textBlock.Details.Align == DisplayBlock.Align.Right ? TextAlignment.Right : TextAlignment.Left
-                            };
-                            if (TryToParseRGB(textBlock.Details.BackColor, out colorHex))
-                            {
-                                label.Background = (Media.Brush)bc.ConvertFrom(colorHex);
-                            }
-                            if (TryToParseRGB(textBlock.Details.TextColor, out colorHex))
-                            {
-                                label.Foreground = (Media.Brush)bc.ConvertFrom(colorHex);
-                            }
-                            Canvas.SetTop(label, textBlock.Top);
-                            Canvas.SetLeft(label, textBlock.Left);
-                            Panel.SetZIndex(label, textBlock.ZIndex);
-                            canvas.Children.Add(label);
+                            canvas.Children.Add(BuildTextBlock(textBlock));
                         }
-                        if (block is DisplayBlock.PictureBlock pictureBlock)
+                        else if (block is DisplayBlock.PictureBlock pictureBlock)
                         {
-                            var bitmap = new BitmapImage();
-                            using (MemoryStream ms = new MemoryStream(Convert.FromBase64String(pictureBlock.Details.Base64Image)))
-                            {
-                                bitmap.BeginInit();
-                                bitmap.CacheOption = BitmapCacheOption.OnLoad; // here
-                                bitmap.StreamSource = ms;
-                                bitmap.EndInit();
-                            }
-
-                            var _image = new Image
-                            {
-                                Height = pictureBlock.Height,
-                                Width = pictureBlock.Width,
-                                Source = bitmap,
-                                Stretch = Media.Stretch.None
-                            };
-
-                            Canvas.SetTop(_image, pictureBlock.Top);
-                            Canvas.SetLeft(_image, pictureBlock.Left);
-                            Panel.SetZIndex(_image, pictureBlock.ZIndex);
-                            canvas.Children.Add(_image);
+                            canvas.Children.Add(BuildImage(pictureBlock));
                         }
-                        if (block is DisplayBlock.TableBlock tableBlock)
+                        else if (block is DisplayBlock.TableBlock tableBlock)
                         {
-                            var dataGrid = new DataGrid
-                            {
-                                HeadersVisibility = DataGridHeadersVisibility.None,
-                                Height = tableBlock.Height,
-                                Width = tableBlock.Width,
-                                BorderThickness = new Thickness(0),
-                                GridLinesVisibility = DataGridGridLinesVisibility.None
-                            };
-                            var columnCount = tableBlock.Details.Cells.Max(c => c.Column) + 1;
-                            for (int i = 0; i < columnCount; i++)
-                            {
-                                DataGridTextColumn column = new DataGridTextColumn();
-                                var name = $"Column{i}";
-                                column.Header = name;
-                                column.Binding = new Binding(name);
-                                dataGrid.Columns.Add(column);
-                            }
-                            foreach (var row in tableBlock.Details.Cells.GroupBy(c => c.Row).OrderBy(r => r.Key))
-                            {
-                                dynamic tableRow = new ExpandoObject();
-                                var i = 0;
-                                foreach (var cell in row.OrderBy(c => c.Column))
-                                {
-                                    var name = $"Column{i}";
-                                    ((IDictionary<string, object>)tableRow)[name] = cell.Value;
-                                    i++;
-                                }
-                                dataGrid.Items.Add(tableRow);
-                            }
-
-
-                            Canvas.SetTop(dataGrid, tableBlock.Top);
-                            Canvas.SetLeft(dataGrid, tableBlock.Left);
-                            Panel.SetZIndex(dataGrid, tableBlock.ZIndex);
-                            canvas.Children.Add(dataGrid);
+                            canvas.Children.Add(BuildTable(tableBlock));
                         }
                     }
 
@@ -211,8 +132,187 @@ namespace Display_control
             notifyIcon.ShowBalloonTip(1, "Display-control", "Сервер запущен и готов к работе", System.Windows.Forms.ToolTipIcon.Info);
         }
 
+        private DataGrid BuildTable(DisplayBlock.TableBlock tableBlock)
+        {
+            var bc = new Media.BrushConverter();
+            string colorHex;
+            var headerStyle = new Style(typeof(System.Windows.Controls.Primitives.DataGridColumnHeader));
+            if (TryToParseRGB(tableBlock.Details.HeaderDetails.TextColor, out colorHex))
+            {
+                headerStyle.Setters.Add(new Setter(Control.ForegroundProperty, (Media.Brush)bc.ConvertFrom(colorHex)));
+            }
+            if (TryToParseRGB(tableBlock.Details.HeaderDetails.BackColor, out colorHex))
+            {
+                headerStyle.Setters.Add(new Setter(Control.BackgroundProperty, (Media.Brush)bc.ConvertFrom(colorHex)));
+            }
+            headerStyle.Setters.Add(new Setter(Control.FontSizeProperty, (double)tableBlock.Details.FontSize));
+            headerStyle.Setters.Add(new Setter(Control.FontFamilyProperty, new Media.FontFamily(tableBlock.Details.FontName)));
+            headerStyle.Setters.Add(new Setter(Control.FontWeightProperty, tableBlock.Details.HeaderDetails.Bold ? FontWeights.Bold : FontWeights.Normal));
+            headerStyle.Setters.Add(new Setter(Control.FontStyleProperty, tableBlock.Details.HeaderDetails.Italic ? FontStyles.Italic : FontStyles.Normal));
+            headerStyle.Setters.Add(new Setter(Control.HorizontalContentAlignmentProperty,
+                    tableBlock.Details.HeaderDetails.Align == DisplayBlock.Align.Left ? HorizontalAlignment.Left :
+                    tableBlock.Details.HeaderDetails.Align == DisplayBlock.Align.Center ? HorizontalAlignment.Center :
+                    tableBlock.Details.HeaderDetails.Align == DisplayBlock.Align.Right ? HorizontalAlignment.Right : HorizontalAlignment.Left));
+
+            var rowStyle = new Style(typeof(DataGridRow));
+            var oddTrigger = new Trigger()
+            {
+                Property = DataGridRow.AlternationIndexProperty,
+                Value = 1
+            };
+            var evenTrigger = new Trigger()
+            {
+                Property = DataGridRow.AlternationIndexProperty,
+                Value = 0
+            };
+            rowStyle.Triggers.Add(oddTrigger);
+            rowStyle.Triggers.Add(evenTrigger);
+#warning Дописать автоподбор высоты строки
+            //rowStyle.Setters.Add(new Setter(DataGridRow.HeightProperty, DataGridLength.Auto));
+
+            if (TryToParseRGB(tableBlock.Details.OddRowDetails.TextColor, out colorHex))
+            {
+                oddTrigger.Setters.Add(new Setter(Control.ForegroundProperty, (Media.Brush)bc.ConvertFrom(colorHex)));
+            }
+            if (TryToParseRGB(tableBlock.Details.OddRowDetails.BackColor, out colorHex))
+            {
+                oddTrigger.Setters.Add(new Setter(Control.BackgroundProperty, (Media.Brush)bc.ConvertFrom(colorHex)));
+            }
+            oddTrigger.Setters.Add(new Setter(Control.FontSizeProperty, (double)tableBlock.Details.FontSize));
+            oddTrigger.Setters.Add(new Setter(Control.FontFamilyProperty, new Media.FontFamily(tableBlock.Details.FontName)));
+            oddTrigger.Setters.Add(new Setter(Control.FontWeightProperty, tableBlock.Details.OddRowDetails.Bold ? FontWeights.Bold : FontWeights.Normal));
+            oddTrigger.Setters.Add(new Setter(Control.FontStyleProperty, tableBlock.Details.OddRowDetails.Italic ? FontStyles.Italic : FontStyles.Normal));
+            oddTrigger.Setters.Add(new Setter(Control.HorizontalContentAlignmentProperty,
+                    tableBlock.Details.OddRowDetails.Align == DisplayBlock.Align.Left ? HorizontalAlignment.Left :
+                    tableBlock.Details.OddRowDetails.Align == DisplayBlock.Align.Center ? HorizontalAlignment.Center :
+                    tableBlock.Details.OddRowDetails.Align == DisplayBlock.Align.Right ? HorizontalAlignment.Right : HorizontalAlignment.Left));
+
+            if (TryToParseRGB(tableBlock.Details.EvenRowDetails.TextColor, out colorHex))
+            {
+                evenTrigger.Setters.Add(new Setter(Control.ForegroundProperty, (Media.Brush)bc.ConvertFrom(colorHex)));
+            }
+            if (TryToParseRGB(tableBlock.Details.EvenRowDetails.BackColor, out colorHex))
+            {
+                evenTrigger.Setters.Add(new Setter(Control.BackgroundProperty, (Media.Brush)bc.ConvertFrom(colorHex)));
+            }
+            evenTrigger.Setters.Add(new Setter(Control.FontSizeProperty, (double)tableBlock.Details.FontSize));
+            evenTrigger.Setters.Add(new Setter(Control.FontFamilyProperty, new Media.FontFamily(tableBlock.Details.FontName)));
+            evenTrigger.Setters.Add(new Setter(Control.FontWeightProperty, tableBlock.Details.EvenRowDetails.Bold ? FontWeights.Bold : FontWeights.Normal));
+            evenTrigger.Setters.Add(new Setter(Control.FontStyleProperty, tableBlock.Details.EvenRowDetails.Italic ? FontStyles.Italic : FontStyles.Normal));
+            evenTrigger.Setters.Add(new Setter(Control.HorizontalContentAlignmentProperty,
+                    tableBlock.Details.EvenRowDetails.Align == DisplayBlock.Align.Left ? HorizontalAlignment.Left :
+                    tableBlock.Details.EvenRowDetails.Align == DisplayBlock.Align.Center ? HorizontalAlignment.Center :
+                    tableBlock.Details.EvenRowDetails.Align == DisplayBlock.Align.Right ? HorizontalAlignment.Right : HorizontalAlignment.Left));
+
+            var dataGrid = new DataGrid
+            {
+                HeadersVisibility = DataGridHeadersVisibility.Column,
+                Height = tableBlock.Height,
+                Width = tableBlock.Width,
+                BorderThickness = new Thickness(0),
+                GridLinesVisibility = DataGridGridLinesVisibility.None,
+                AlternationCount = 2,
+                ColumnHeaderStyle = headerStyle,
+                RowStyle = rowStyle
+            };
+            var columnCount = tableBlock.Details.Cells.Max(c => c.Column) + 1;
+            var rows = tableBlock.Details.Cells.GroupBy(c => c.Row).OrderBy(r => r.Key);
+            var header = rows.FirstOrDefault().OrderBy(c => c.Column).ToArray();
+            for (int i = 0; i < columnCount; i++)
+            {
+                var name = $"Column{i}";
+                dataGrid.Columns.Add(new DataGridTextColumn
+                {
+                    Header = header[i].Value,
+                    Binding = new Binding(name),
+                    Width = new DataGridLength(1, DataGridLengthUnitType.Star)
+                });
+            }
+            foreach (var row in rows.Skip(1))
+            {
+                dynamic tableRow = new ExpandoObject();
+                var i = 0;
+                foreach (var cell in row.OrderBy(c => c.Column))
+                {
+                    var name = $"Column{i}";
+                    ((IDictionary<string, object>)tableRow)[name] = cell.Value;
+                    i++;
+                }
+                dataGrid.Items.Add(tableRow);
+            }
+
+
+            Canvas.SetTop(dataGrid, tableBlock.Top);
+            Canvas.SetLeft(dataGrid, tableBlock.Left);
+            Panel.SetZIndex(dataGrid, tableBlock.ZIndex);
+
+            return dataGrid;
+        }
+
+        private static Image BuildImage(DisplayBlock.PictureBlock pictureBlock)
+        {
+            var bitmap = new BitmapImage();
+            using (MemoryStream ms = new MemoryStream(Convert.FromBase64String(pictureBlock.Details.Base64Image)))
+            {
+                bitmap.BeginInit();
+                bitmap.CacheOption = BitmapCacheOption.OnLoad; // here
+                bitmap.StreamSource = ms;
+                bitmap.EndInit();
+            }
+
+            var _image = new Image
+            {
+                Height = pictureBlock.Height,
+                Width = pictureBlock.Width,
+                Source = bitmap,
+                Stretch = Media.Stretch.None
+            };
+
+            Canvas.SetTop(_image, pictureBlock.Top);
+            Canvas.SetLeft(_image, pictureBlock.Left);
+            Panel.SetZIndex(_image, pictureBlock.ZIndex);
+            return _image;
+        }
+
+        private TextBlock BuildTextBlock(DisplayBlock.TextBlock textBlock)
+        {
+            var bc = new Media.BrushConverter();
+            string colorHex;
+            var label = new TextBlock
+            {
+                Height = textBlock.Height,
+                Width = textBlock.Width,
+                Text = textBlock.Details.Text,
+                FontSize = textBlock.Details.FontSize,
+                FontFamily = new Media.FontFamily(textBlock.Details.FontName),
+                FontWeight = textBlock.Details.Bold ? FontWeights.Bold : FontWeights.Normal,
+                FontStyle = textBlock.Details.Italic ? FontStyles.Italic : FontStyles.Normal,
+                LineHeight = textBlock.Details.FontSize * textBlock.Details.FontIndex,
+                TextAlignment = textBlock.Details.Align == DisplayBlock.Align.Left ? TextAlignment.Left :
+                                                textBlock.Details.Align == DisplayBlock.Align.Center ? TextAlignment.Center :
+                                                textBlock.Details.Align == DisplayBlock.Align.Right ? TextAlignment.Right : TextAlignment.Left
+            };
+            if (TryToParseRGB(textBlock.Details.BackColor, out colorHex))
+            {
+                label.Background = (Media.Brush)bc.ConvertFrom(colorHex);
+            }
+            if (TryToParseRGB(textBlock.Details.TextColor, out colorHex))
+            {
+                label.Foreground = (Media.Brush)bc.ConvertFrom(colorHex);
+            }
+            Canvas.SetTop(label, textBlock.Top);
+            Canvas.SetLeft(label, textBlock.Left);
+            Panel.SetZIndex(label, textBlock.ZIndex);
+            return label;
+        }
+
         private bool TryToParseRGB(string colorString, out string colorHexString)
         {
+            if (Regex.IsMatch(colorString, @"\#\w{6}"))
+            {
+                colorHexString = colorString;
+                return true;
+            }
             var regular = @"^rgb\(\s?(\d{1,3})\,\s?(\d{1,3})\,\s?(\d{1,3})\)$";
             colorHexString = string.Empty;
             if (Regex.IsMatch(colorString, regular))
