c3ac44ca30ee59b895875da3b0b052b0aecd790f
Жуков Алексей <mantis-religiosa@yandex.ru>
2019-02-09 12:20:08 +0400

    Реверт версии

--- Display-control/Properties/AssemblyInfo.cs
+++ Display-control/Properties/AssemblyInfo.cs
@@ -6,8 +6,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.6")]
-[assembly: AssemblyFileVersion("1.0.6")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
 [assembly: AssemblyInformationalVersion("1.0.6")]
 [assembly: AssemblyCopyright("Copyright (c) 2019")]
 [assembly: Guid("01C10720-3D90-417C-80AC-5D7841718D12")]
