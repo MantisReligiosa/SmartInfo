7dd43d77549d972dc2497efdc1fe6ac93d9aadfd
Жуков Алексей <mantis-religiosa@yandex.ru>
2019-02-11 22:03:25 +0400

    Release 0.0.2

--- Display-control/Properties/AssemblyInfo.cs
+++ Display-control/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
-[assembly: AssemblyInformationalVersion("0.0.1")]
+[assembly: AssemblyVersion("0.0.2")]
+[assembly: AssemblyFileVersion("0.0.2")]
+[assembly: AssemblyInformationalVersion("0.0.2")]
 [assembly: AssemblyCopyright("Copyright (c) 2019")]
 [assembly: Guid("01C10720-3D90-417C-80AC-5D7841718D12")]
 
