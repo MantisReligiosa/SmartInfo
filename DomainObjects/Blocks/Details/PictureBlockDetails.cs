509e7139e31edd9cb65252e2925f0a551c3f983a
Алексей Жуков <mantis-religiosa@yandex.ru>
2020-08-16 23:21:43 +0400

    dc-62 миграция

--- DomainObjects/Blocks/Details/PictureBlockDetails.cs
+++ DomainObjects/Blocks/Details/PictureBlockDetails.cs
@@ -1,12 +1,15 @@
-﻿using System;
-
-namespace DomainObjects.Blocks.Details
+﻿namespace DomainObjects.Blocks.Details
 {
     public class PictureBlockDetails : Identity, ICopyable<PictureBlockDetails>
     {
         public string Base64Image { get; set; }
 
-        public PictureBlockDetails() { }
+        public ImageMode ImageMode { get; set; }
+
+        public bool SaveProportions { get; set; }
+
+        public PictureBlockDetails()
+        { }
 
         public PictureBlockDetails(PictureBlockDetails source)
         {
@@ -16,6 +19,14 @@ namespace DomainObjects.Blocks.Details
         public void CopyFrom(PictureBlockDetails sourceDetails)
         {
             Base64Image = sourceDetails.Base64Image;
+            ImageMode = sourceDetails.ImageMode;
+            SaveProportions = sourceDetails.SaveProportions;
         }
+    }
+
+    public enum ImageMode
+    {
+        Crop = 0,
+        Zoom = 1
     }
 }
