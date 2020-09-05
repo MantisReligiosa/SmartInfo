7120ddd096a4ed744ca1158bd6dc12e201049871
Алексей Жуков <mantis-religiosa@yandex.ru>
2020-09-05 23:52:35 +0400

    si-165

--- Setup/PackageBuilders/SqlExpressPackageBuilder.cs
+++ Setup/PackageBuilders/SqlExpressPackageBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using Setup.Interfaces;
 using System;
 using System.Net;
+using WixSharp;
 using WixSharp.Bootstrapper;
 
 namespace Setup.Packages
@@ -21,10 +22,9 @@ namespace Setup.Packages
                 DisplayName = "Installing Microsoft® SQL Server® 2019 Express Edition",
                 Compressed = true,
                 InstallCommand = $"'[WixBundleLastUsedSource]{_sourceFile}' /q /CONFIGURATIONFILE=SqlExpress.ini /IAcceptSQLServerLicenseTerms",
-                InstallCondition = "Not SqlInstanceFoundx64",
-                DetectCondition = "SqlInstanceFoundx64",
+                DetectCondition = "SQLSERVERINSTALLED",
                 PerMachine = true,
-                Payloads = new Payload[] { new Payload("SqlExpress.ini"), new Payload(_sourceFile) }
+                Payloads = new Payload[] { new Payload("SqlExpress.ini"), new Payload(_sourceFile) },
             };
             return package;
         }
