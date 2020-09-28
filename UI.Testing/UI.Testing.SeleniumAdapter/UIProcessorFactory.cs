112ed871fd76c80b6f55c74163b65e3a08a2872d
Алексей Жуков <mantis-religiosa@yandex.ru>
2020-09-29 00:03:19 +0400

    Selenium_tests

---
+++ UI.Testing/UI.Testing.SeleniumAdapter/UIProcessorFactory.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Testing.SeleniumAdapter
+{
+    public class UIProcessorFactory
+    {
+        public UIProcessor Get(RemoteWebDriver driver, Page page)
+        {
+            return null;
+        }
+    }
+}
