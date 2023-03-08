d45c4231d8d5b61961c7712b0916f672e5711aac
Aleksey Zhukov <a.zhukow@dodopizza.com>
2023-03-08 21:06:30 +0300

    Content, favicon

--- SmartInfo.Web/Pipelines/ApplicationBuilderExtensions.cs
+++ SmartInfo.Web/Pipelines/ApplicationBuilderExtensions.cs
@@ -11,6 +11,12 @@ internal static class ApplicationBuilderExtensions
             FileProvider = new PhysicalFileProvider(Path.Combine(contentRootPath, "vue-app/dist")),
             RequestPath = "/dist"
         });
+        
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            FileProvider = new PhysicalFileProvider(Path.Combine(contentRootPath, "Content")),
+            RequestPath = "/content"
+        });
 
         return app;
     }
