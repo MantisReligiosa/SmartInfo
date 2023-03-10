15ec0192d7006dbae1b3cc5496e0b398d814d808
Aleksey Zhukov <a.zhukow@dodopizza.com>
2023-03-10 20:00:06 +0300

    Components

--- SmartInfo.Web/vue-app/src/App.vue
+++ SmartInfo.Web/vue-app/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PreLoader />
+  <MainPanel />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainPanel from './components/MainPanel.vue'
+import PreLoader from "@/components/PreLoader.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainPanel,
+    PreLoader
   }
 }
 </script>
---
+++ SmartInfo.Web/vue-app/src/components/MainPanel.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="mainPanel">
+     <ul>
+     </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PreLoader"
+}
+</script>
+
+<style scoped>
+
+</style>
