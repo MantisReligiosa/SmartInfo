be9cbd5b8b776f9592086450becc773c6264a61f
Aleksey Zhukov <a.zhukow@dodopizza.com>
2023-03-13 21:28:59 +0300

    Main panel

--- SmartInfo.Web/vue-app/src/App.vue
+++ SmartInfo.Web/vue-app/src/App.vue
@@ -1,17 +1,33 @@
 <template>
-  <PreLoader />
-  <MainPanel />
+  <v-card>
+    <v-layout>
+      <MainPanel></MainPanel>
+      
+
+      <v-navigation-drawer color="surface">
+        <v-list>
+          <v-list-item title="Drawer left"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-navigation-drawer color="surface" location="right">
+        <v-list>
+          <v-list-item title="Drawer right"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main></v-main>
+    </v-layout>
+  </v-card>
 </template>
 
 <script>
-import MainPanel from './components/MainPanel.vue'
-import PreLoader from "@/components/PreLoader.vue";
 
+import MainPanel from './components/MainPanel.vue'
 export default {
   name: 'App',
   components: {
-    MainPanel,
-    PreLoader
+    MainPanel
   }
 }
 </script>
--- SmartInfo.Web/vue-app/src/components/MainPanel.vue
+++ SmartInfo.Web/vue-app/src/components/MainPanel.vue
@@ -1,19 +1,16 @@
 <template>
-  <v-tabs bg-color="primary">
-    <v-tab>One</v-tab>
-    <v-tab>Two</v-tab>
-  </v-tabs>
+  <v-app-bar
+      color="primary">
+    <v-app-bar-nav-icon variant="text"></v-app-bar-nav-icon>
+<!--    <v-app-bar-title>Smart info</v-app-bar-title>-->
+    <v-btn variant="text">
+      <v-icon icon="mdi-navigation-variant-outline" class="mdi-flip-h" size="x-large"></v-icon>
+    </v-btn>
+  </v-app-bar>
 </template>
 
 <script>
- import {VTabs, VTab} from 'vuetify/components/VTabs'
 
- export default {
-   components: {
-     VTabs,
-     VTab
-   }
- }
 </script>
 
 <style scoped>
