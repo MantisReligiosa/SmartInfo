69b75f984c4281cbbdb88e2a6d25780beccbc60e
Aleksey Zhukov <a.zhukow@dodopizza.com>
2023-07-21 14:00:06 +0300

    Presentation

--- SmartInfo.Web/vue-app/src/components/Presentation/TextBlock.vue
+++ SmartInfo.Web/vue-app/src/components/Presentation/TextBlock.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import {computed, defineProps} from "vue";
+import {ITextBlock} from "@/interfaces/Blocks";
 
+const props = defineProps(['block'])
+const block = computed(() => props.block as ITextBlock)
 </script>
 
 <template>
-<div>Text</div>
+<div>{{block?.text}}</div>
 </template>
 
 <style scoped>
--- SmartInfo.Web/vue-app/src/components/WorkSpace.vue
+++ SmartInfo.Web/vue-app/src/components/WorkSpace.vue
@@ -1,26 +1,38 @@
 <template>
-    <v-container
-            class="ml-0 ma-1 h-100 workSpace"
+  <v-container
+      class="ml-0 ma-1 h-100 workSpace"
+  >
+    <v-card
+        color="black"
+        :height="h + 2 * padding"
+        :width="w + 2 * padding"
+        :style="{'padding-left': padding + 'px', 'padding-top': padding + 'px'}"
+        class="elevation-5"
     >
-        <v-card
-                color="black"
-                :height="h + 2 * padding"
-                :width="w + 2 * padding"
-                :style="{'padding-left': padding + 'px', 'padding-top': padding + 'px'}"
-                class="elevation-5"
-        >
-            <v-card
-                    :height="h"
-                    :width="w"
-                    :color="device.backColor"
-                    rounded="0"
-                    class="pa-0 ma-0"
-            >
-              <component v-for="block in blocks" v-bind:key="block" :is="block"></component>
-
-            </v-card>
-        </v-card>
-    </v-container>
+      <v-card
+          :height="h"
+          :width="w"
+          :color="device.backColor"
+          rounded="0"
+          class="pa-0 ma-0"
+      >
+        <component
+            v-for="(block,index) in blocks"
+            v-bind:key="index"
+            :is="BuildPresentationBlock(block)"
+            v-bind:block="blocks[index]"
+            :style="{
+              'left': block.x * scale + 'px',
+              'top': block.y * scale + 'px',
+              'height': block.h * scale + 'px',
+              'width': block.w * scale + 'px',
+              'position': 'absolute',
+              'border': '1px black solid'
+            }"
+        ></component>
+      </v-card>
+    </v-card>
+  </v-container>
 </template>
 
 <script setup lang="ts">
@@ -32,6 +44,7 @@ import {BuildPresentationBlock} from "@/components/Presentation/PresentationBloc
 
 const mStore = mainStore()
 const dStore = deviceStore()
+const scale = computed(() => mStore.scale)
 
 const h = computed(() => dStore.resolution.height * mStore.scale)
 const w = computed(() => dStore.resolution.width * mStore.scale)
@@ -44,16 +57,16 @@ const blocks = computed(() => {
     return []
   }
 
-  return device.value.blocks.map(b => BuildPresentationBlock(b))
+  return device.value.blocks
 })
 </script>
 
 <style scoped>
 .workSpace {
-    background-color: #eeeeee;
-    max-width: 100% !important;
-    overflow-x: scroll;
-    overflow-y: scroll;
+  background-color: #eeeeee;
+  max-width: 100% !important;
+  overflow-x: scroll;
+  overflow-y: scroll;
 }
 
 </style>
