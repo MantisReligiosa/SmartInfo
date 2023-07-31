2414955a68cfcbcf701044ca1944dcb3ba31a594
Aleksey Zhukov <a.zhukow@dodopizza.com>
2023-07-31 18:11:10 +0300

    Text animation

--- SmartInfo.Web/vue-app/src/components/Presentation/TextBlock.vue
+++ SmartInfo.Web/vue-app/src/components/Presentation/TextBlock.vue
@@ -5,18 +5,78 @@ import PresentationBlock from "@/components/Presentation/PresentationBlock.vue";
 
 const props = defineProps(['block', 'scale'])
 const block = computed(() => props.block as ITextBlock)
+const isAnimationEnabled = computed(() => block.value.isAnimationEnabled)
+const animationName = computed(() => {
+  switch (block.value.animationStyle) {
+    case 0:
+      return 'ltrAnimation';
+    case 1:
+      return 'rtlAnimation';
+    default:
+      return 'utdAnimation';
 
+  }
+})
+const animationDuration = computed(() => (11-block.value.animationSpeed)+'s')
 </script>
 
 <template>
   <presentation-block
+      class="block"
       v-bind:block="props.block"
       v-bind:scale="props.scale"
   >
-    <div class="vAlignStyle">{{block?.text}}</div>
+    <div
+        class="vAlignStyle h-100"
+        :class="{marquee:isAnimationEnabled}"
+    >{{ block?.text }}
+    </div>
   </presentation-block>
 </template>
 
 <style scoped>
+.block {
+  overflow: hidden;
+}
 
+.marquee {
+  animation-name: v-bind(animationName);
+  //animation-duration: 5s;
+  animation-iteration-count: infinite;
+  animation-timing-function: ease;
+  animation-play-state: running;
+  animation-duration: v-bind(animationDuration);
+  //animation: 5s ease infinite;
+}
+
+
+</style>
+
+<style>
+@keyframes ltrAnimation {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(100%);
+  }
+}
+
+@keyframes rtlAnimation {
+  0% {
+    transform: translateX(100%);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+
+@keyframes utdAnimation {
+  0% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(100%);
+  }
+}
 </style>
