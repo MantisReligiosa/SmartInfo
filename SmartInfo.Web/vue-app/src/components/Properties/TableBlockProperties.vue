2b38e94079ad8798b154ecc9894118f9c1711ab0
Aleksey Zhukov <a.zhukow@dodopizza.com>
2023-11-25 09:35:21 +0300

    New fontProperties, table properties

--- SmartInfo.Web/vue-app/src/components/Properties/TableBlockProperties.vue
+++ SmartInfo.Web/vue-app/src/components/Properties/TableBlockProperties.vue
@@ -1,3 +1,25 @@
 <template>
-TABLE
+    <v-card
+    title="Заголовок">
+        <font-properties-new
+            :font="headerFont"
+            @change="onHeaderChange"
+        ></font-properties-new>
+    </v-card>
 </template>
+
+<script setup lang="ts">
+import {deviceStore} from "@/store/deviceStore";
+import {computed} from "vue";
+import {IBlockFont, ITableBlock} from "@/interfaces/Blocks";
+import FontPropertiesNew from "@/components/Properties/Common/FontPropertiesNew.vue";
+
+const dStore = deviceStore()
+const block = computed(() => dStore.block as ITableBlock)
+const headerFont = computed(() => block.value.headerDetails)
+
+const onHeaderChange = function (font: IBlockFont) {
+  console.log(font)
+  //block.value.headerDetails = font
+}
+</script>
